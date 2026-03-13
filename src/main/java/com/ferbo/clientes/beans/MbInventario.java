ac3eaade018ccc01694419919955fcde96a0d817
calberto <rinoa_heartili@hotmail.com>
2026-03-13 12:33:48 -0600

    Se agrega el modulo de reportes

--- src/main/java/com/ferbo/clientes/beans/MbInventario.java
+++ src/main/java/com/ferbo/clientes/beans/MbInventario.java
@@ -1,14 +1,14 @@
 package com.ferbo.clientes.beans;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.InputStream;
 import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.context.FacesContext;
@@ -19,10 +19,12 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
 
 import com.ferbo.clientes.model.Cliente;
 import com.ferbo.clientes.util.Conexion;
-import com.ferbo.clientes.util.JasperReportUtil;
+import com.ferbo.gestion.reports.jasper.ReporteInventarioJR;
 
 @Named(value = "mbInventario")
 @ViewScoped
@@ -38,15 +40,18 @@ public class MbInventario implements Serializable {
 	private HttpServletRequest request;
 	private HttpSession session;
 	private Cliente cliente;
+        private ReporteInventarioJR reporteInventarioJR;
+        private StreamedContent file;
 	
 	@PostConstruct
 	public void init() {
 		this.fechaCorte = new Date();
 		
 		faceContext = FacesContext.getCurrentInstance();
-        request = (HttpServletRequest) faceContext.getExternalContext().getRequest();
-        session = request.getSession(false);
+                request = (HttpServletRequest) faceContext.getExternalContext().getRequest();
+                session = request.getSession(false);
 		this.cliente = (Cliente) session.getAttribute("cliente");
+                log.info("El cliente {} ha ingresado a reportes de inventario.", this.cliente.getNombre());
 	}
 	
 	public void generateReport() {
@@ -58,18 +63,14 @@ public class MbInventario implements Serializable {
 		File logoFile = new File(getClass().getResource(sLogoPath).getFile());
 		log.info("Imagen: " + logoFile.getPath());
 		
-		JasperReportUtil jasperReportUtil = new JasperReportUtil();
-		Map<String, Object> parameters = new HashMap<String, Object>();
 		Connection connection = null;
-		parameters = new HashMap<String, Object>();
 		try {
 			connection = Conexion.getConnection();
-			parameters.put("REPORT_CONNECTION", connection);
-			parameters.put("idCliente", this.cliente.getIdCliente());
-			parameters.put("fecha", getFechaCorte());
-			parameters.put("imagen", logoFile.getPath());
-			log.info("Parametros: " + parameters.toString());
-			jasperReportUtil.createPdf(getNameFilePdf(), parameters, reportFile.getPath());
+                        reporteInventarioJR = new ReporteInventarioJR(connection, sLogoPath);
+                        byte[] bytes = reporteInventarioJR.getPDFReporteInventario(this.cliente.getIdCliente(), null);
+                        InputStream input = new ByteArrayInputStream(bytes);
+                        this.file = DefaultStreamedContent.builder().contentType("application/pdf").name(getNameFilePdf()).stream(() -> input).build();
+                        log.info("Terminando generacion de reporte de inventarios...");
 		} catch (SQLException ex) {
 			log.error("Problema en base de datos...", ex);
 		} catch(Exception ex) {
@@ -81,8 +82,9 @@ public class MbInventario implements Serializable {
 	
 	public String getNameFilePdf() {
 		DateFormat dateFormatPeriodo = new SimpleDateFormat("yyyy-mm-dd"); 
-		String strDatePeriodo = dateFormatPeriodo.format(getFechaCorte());  
-		return "inventario"+"_"+strDatePeriodo+".pdf";
+		String strDatePeriodo = dateFormatPeriodo.format(getFechaCorte());
+                String nombreArchivo = String.format("Inventario_%s.pdf", strDatePeriodo);
+		return nombreArchivo;
 	}
 	
 	public Date getFechaCorte() {
@@ -92,5 +94,13 @@ public class MbInventario implements Serializable {
 	public void setFechaCorte(Date periodo) {
 		this.fechaCorte = periodo;
 	}
+
+        public StreamedContent getFile() {
+            return file;
+        }
+
+        public void setFile(StreamedContent file) {
+            this.file = file;
+        }
 
 }
